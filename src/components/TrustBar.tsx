9ac01fcd26fd5a1838386ff471f9fd056d322e50
Mario <torosilva@gmail.com>
2026-01-05 22:49:08 -0600

    style: solve white space disaster with zoom-and-crop logic

--- src/components/TrustBar.tsx
+++ src/components/TrustBar.tsx
@@ -13,15 +13,15 @@ export const TrustBar: React.FC = () => {
                 zIndex: 5
             }}
         >
-            <div style={{
+            <div className="container" style={{
                 display: 'flex',
                 flexDirection: 'column',
-                gap: '1.5rem',
+                gap: '0.5rem',
                 alignItems: 'center',
                 width: '100%'
             }}>
 
-                {/* Ecosystem Segment */}
+                {/* Ecosystem Segment - "Zooming" in to kill the PNG whitespace */}
                 <div style={{ width: '100%', textAlign: 'center' }}>
                     <h4 style={{
                         fontSize: '0.65rem',
@@ -29,49 +29,81 @@ export const TrustBar: React.FC = () => {
                         color: '#94A3B8',
                         letterSpacing: '0.2em',
                         textTransform: 'uppercase',
-                        margin: '0 0 0.5rem 0'
+                        margin: '0 0 -3.5rem 0', // Pulling image up
+                        position: 'relative',
+                        zIndex: 2
                     }}>
                         Ecosystem
                     </h4>
-                    <div style={{ display: 'flex', justifyContent: 'center', opacity: 0.7, filter: 'grayscale(100%)' }}>
+                    <div style={{
+                        width: '100%',
+                        height: '140px', // Visible window
+                        overflow: 'hidden',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        opacity: 0.8
+                    }}>
                         <img
                             src={partnerLogos}
                             alt="Ecosystem Partners"
                             style={{
-                                height: '40px',
+                                height: '400px', // Dramatically scale up to fill the 140px window and hide margins
                                 width: 'auto',
-                                maxWidth: '90vw'
+                                marginTop: '20px', // Center the logos vertically in the crop
+                                filter: 'grayscale(100%) contrast(1.1)',
+                                objectFit: 'contain'
                             }}
                         />
                     </div>
                 </div>
 
-                {/* Clients Segment */}
-                <div style={{ width: '100%', textAlign: 'center' }}>
+                {/* Clients Segment - "Zooming" in to kill the PNG whitespace */}
+                <div style={{ width: '100%', textAlign: 'center', marginTop: '-3rem' }}>
                     <h4 style={{
                         fontSize: '0.65rem',
                         fontWeight: 900,
                         color: '#94A3B8',
                         letterSpacing: '0.2em',
                         textTransform: 'uppercase',
-                        margin: '0 0 0.5rem 0'
+                        margin: '0 0 -4.5rem 0', // Pulling image up
+                        position: 'relative',
+                        zIndex: 2
                     }}>
                         Clientes
                     </h4>
-                    <div style={{ display: 'flex', justifyContent: 'center', opacity: 0.7, filter: 'grayscale(100%)' }}>
+                    <div style={{
+                        width: '100%',
+                        height: '140px', // Visible window
+                        overflow: 'hidden',
+                        display: 'flex',
+                        alignItems: 'center',
+                        justifyContent: 'center',
+                        opacity: 0.8
+                    }}>
                         <img
                             src={clientLogos}
                             alt="Nuestros Clientes"
                             style={{
-                                height: '45px',
+                                height: '500px', // Dramatically scale up to hide margins
                                 width: 'auto',
-                                maxWidth: '90vw'
+                                marginTop: '10px', // Center the logos vertically in the crop
+                                filter: 'grayscale(100%) contrast(1.1)',
+                                objectFit: 'contain'
                             }}
                         />
                     </div>
                 </div>
 
             </div>
+
+            <style>{`
+                img:hover {
+                    opacity: 1 !important;
+                    filter: grayscale(0%) brightness(1) !important;
+                    transition: all 0.4s ease;
+                }
+            `}</style>
         </section>
     );
 };
